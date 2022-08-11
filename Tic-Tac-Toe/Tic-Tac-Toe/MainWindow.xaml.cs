fa70195b01f4c62a0cd681b4d45f9479a994dcfc
ulugbek03 <keldibekovulugbek@gmail.com>
2022-08-11 21:49:53 +0500

    first commit

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tic_Tac_Toe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Person = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SinglePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            Person = 1;
+            Main.Content = new Page1();
+        }
+        private void SecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            Person = 2;
+            Main.Content = new Page1();
+        }
+    }
+}
